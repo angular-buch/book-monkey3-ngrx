ddf2195c77c36ed8be5e3b8602beafde28b697ad
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-06-04 22:03:26 +0200

    Add effect for loading books

--- src/app/books/effects/book.effects.ts
+++ src/app/books/effects/book.effects.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { catchError, map, concatMap } from 'rxjs/operators';
-import { EMPTY, of } from 'rxjs';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
+
 import { LoadBooksFailure, LoadBooksSuccess, BookActionTypes, BookActions } from '../actions/book.actions';
-
-
+import { BookStoreService } from 'src/app/shared/book-store.service';
 
 @Injectable()
 export class BookEffects {
@@ -12,15 +12,16 @@ export class BookEffects {
   @Effect()
   loadBooks$ = this.actions$.pipe(
     ofType(BookActionTypes.LoadBooks),
-    concatMap(() =>
-      /** An EMPTY observable only emits completion. Replace with your own observable API request */
-      EMPTY.pipe(
-        // map(data => new LoadBooksSuccess({ data })),
+    switchMap(() =>
+      this.bs.getAll().pipe(
+        map(books => new LoadBooksSuccess({ books })),
         catchError(error => of(new LoadBooksFailure({ error }))))
     )
   );
 
-
-  constructor(private actions$: Actions<BookActions>) {}
+  constructor(
+    private actions$: Actions<BookActions>,
+    private bs: BookStoreService
+  ) {}
 
 }
