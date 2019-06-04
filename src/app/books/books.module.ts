ed5757ad0aa40da90536fcefb4153a161058e776
Ferdinand Malcher <mail@ferdinand-malcher.de>
2019-06-04 21:35:44 +0200

    Generate "book" feature
    ng g feature books/book --module books/books --group --api

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -8,11 +8,17 @@ import { BookListItemComponent } from './book-list-item/book-list-item.component
 import { BookDetailsComponent } from './book-details/book-details.component';
 import { IsbnPipe } from './shared/isbn.pipe';
 import { ZoomDirective } from './shared/zoom.directive';
+import { StoreModule } from '@ngrx/store';
+import * as fromBook from './reducers/book.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { BookEffects } from './effects/book.effects';
 
 @NgModule({
   imports: [
     CommonModule,
-    BooksRoutingModule
+    BooksRoutingModule,
+    StoreModule.forFeature('book', fromBook.reducer),
+    EffectsModule.forFeature([BookEffects])
   ],
   declarations: [
     BookListComponent,
